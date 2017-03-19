70d3cbfa8761119e22a963261a9eec99047ccbfb
Ulysses <udevide@gmail.com>
2017-03-19 17:17:05 -0400

    Update dvz_mentions_alerts_post.lang.php

--- russian/dvz_mentions_alerts_post.lang.php
+++ russian/dvz_mentions_alerts_post.lang.php
@@ -2,5 +2,5 @@
 /* by Tomasz 'Devilshakerz' Mlynski [devilshakerz.com]; Copyright (C) 2014-2016
  released under Creative Commons BY-NC-SA 4.0 license: http://creativecommons.org/licenses/by-nc-sa/4.0/ */
 
-$l['myalerts_dvz_mentions_post_alert'] = '{1} mentioned you in "{2}".';
-$l['myalerts_setting_dvz_mentions_post'] = 'Receive alert when mentioned in a post?';
+$l['myalerts_dvz_mentions_post_alert'] = '{1} упомянул вас в "{2}".';
+$l['myalerts_setting_dvz_mentions_post'] = 'Получать уведомления, когда вас упоминают в сообщениях?';
