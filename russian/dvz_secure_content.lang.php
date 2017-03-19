930d5ce8174ceebc4864f4b72c31c4f8a06a52e0
Ulysses <udevide@gmail.com>
2017-03-19 17:09:21 -0400

    Update dvz_secure_content.lang.php

--- russian/dvz_secure_content.lang.php
+++ russian/dvz_secure_content.lang.php
@@ -2,4 +2,4 @@
 /* by Tomasz 'Devilshakerz' Mlynski [devilshakerz.com]; Copyright (C) 2015-2017
  released under Creative Commons BY-NC-SA 4.0 license: https://creativecommons.org/licenses/by-nc-sa/4.0/ */
 
-$l['dvz_sc_avatars_https_only'] = '<br /><b>The avatar URL has to begin with <i>https://</i>.</b>';
+$l['dvz_sc_avatars_https_only'] = '<br /><b>Ссылка на аватар должна начинаться с <i>https://</i>.</b>';
