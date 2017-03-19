dcafdd087fd1abc870efe6710fd1d33a864887ae
Ulysses <udevide@gmail.com>
2017-03-19 17:16:02 -0400

    Update myalerts.lang.php

--- russian/myalerts.lang.php
+++ russian/myalerts.lang.php
@@ -1,48 +1,48 @@
 <?php
-$l['myalerts'] = 'MyAlerts';
+$l['myalerts'] = 'Мои уведомления';
 
-$l['myalerts_page_title'] = 'Recent Alerts';
-$l['myalerts_page_getnew'] = 'Check for new alerts';
+$l['myalerts_page_title'] = 'Недавние уведомления';
+$l['myalerts_page_getnew'] = 'Проверить наличие новых уведомлений';
 
-$l['myalerts_alerts'] = 'Alerts';
+$l['myalerts_alerts'] = 'Уведомления';
 
-$l['myalerts_settings_page_title'] = 'Alert Settings';
+$l['myalerts_settings_page_title'] = 'Параметры уведомлений';
 
-$l['myalerts_online_location_listing'] = 'Viewing Alerts';
+$l['myalerts_online_location_listing'] = 'Просматривает уведомления';
 
-$l['myalerts_unread_title'] = 'Unread Alerts';
+$l['myalerts_unread_title'] = 'Пометить уведомления как непрочитанные';
 
-$l['myalerts_no_alerts'] = 'No alerts to display';
-$l['myalerts_rep'] = '{1} modified your reputation.';
-$l['myalerts_pm'] = '{1} sent you a new private message titled "{2}".';
-$l['myalerts_buddylist'] = '{1} added you to their buddy list.';
-$l['myalerts_quoted'] = '{1} quoted you in {2}.';
-$l['myalerts_post_threadauthor'] = '{1} replied to your thread "{2}". There may be more posts after this.';
-$l['myalerts_subscribed_thread'] = '{1} replied to your subscribed thread "{2}".';
+$l['myalerts_no_alerts'] = 'Нет уведомления';
+$l['myalerts_rep'] = '{1} изменил вашу репутацию.';
+$l['myalerts_pm'] = '{1} прислал вам новое личное сообщение, озаглавленное "{2}".';
+$l['myalerts_buddylist'] = '{1} добавил вас в список друзей.';
+$l['myalerts_quoted'] = '{1} процитировал вас в {2}.';
+$l['myalerts_post_threadauthor'] = '{1} ответил в вашей теме "{2}". Возможно, там есть еще сообщения.';
+$l['myalerts_subscribed_thread'] = '{1} ответил в теме, на которую вы подписаны "{2}".';
 
-$l['myalerts_setting_rep'] = 'Receive alert for reputation?';
-$l['myalerts_setting_pm'] = 'Receive alert for Private Message (PM)?';
-$l['myalerts_setting_buddylist'] = 'Receive alert when added to buddylist?';
-$l['myalerts_setting_quoted'] = 'Receive alert when quoted in a post?';
-$l['myalerts_setting_post_threadauthor'] = 'Receive alert when somebody replies to your thread?';
-$l['myalerts_setting_subscribed_thread'] = 'Receive alert when somebody replies to one of your subscribed threads?';
-$l['myalerts_settings_save'] = 'Save Settings';
-$l['myalerts_settings_updated'] = 'Alert settings updated successfully. Redirecting back to settings page.';
-$l['myalerts_settings_updated_title'] = 'Alert Settings Updated.';
+$l['myalerts_setting_rep'] = 'Получать уведомления об изменении репутации?';
+$l['myalerts_setting_pm'] = 'Получать уведомления о личных сообщениях?';
+$l['myalerts_setting_buddylist'] = 'Получать уведомления о добавлении в друзья?';
+$l['myalerts_setting_quoted'] = 'Получать уведомления, когда ваше сообщение цитируют?';
+$l['myalerts_setting_post_threadauthor'] = 'Получать уведомления, когда в вашу тему отвечают?';
+$l['myalerts_setting_subscribed_thread'] = 'Получать уведомления, когда отвечают в одну из тем, на которые вы подписаны?';
+$l['myalerts_settings_save'] = 'Сохранить параметры';
+$l['myalerts_settings_updated'] = 'Параметры уведомлений успешно изменены. Вы будете возвращены на страницу настроек.';
+$l['myalerts_settings_updated_title'] = 'Параметры уведомлений обновлены.';
 
-$l['myalerts_delete_deleted'] = 'Alert deleted successfully.';
-$l['myalerts_delete_error'] = 'Alert cannot be deleted at this time.';
-$l['myalerts_delete_read_confirm'] = 'Really delete all read alerts? This cannot be undone!';
-$l['myalerts_delete_all_confirm'] = 'Really delete all alerts? This cannot be undone!';
-$l['myalerts_delete_mass_deleted'] = 'Alerts Deleted';
-$l['myalerts_delete_all'] = 'All alerts deleted successfully.';
+$l['myalerts_delete_deleted'] = 'Уведомление удалено.';
+$l['myalerts_delete_error'] = Уведомление не может быть удалено в данный момент.';
+$l['myalerts_delete_read_confirm'] = 'Пометить все уведомления как прочитанные? Это действие невозможно отменить!';
+$l['myalerts_delete_all_confirm'] = 'Удалить все уведомления? Это действие невозможно отменить!';
+$l['myalerts_delete_mass_deleted'] = 'Уведомления удалены';
+$l['myalerts_delete_all'] = 'Все уведомления удалены.';
 
-$l['myalerts_usercp_nav'] = 'Alerts';
-$l['myalerts_usercp_nav_alerts'] = 'View Alerts';
-$l['myalerts_usercp_nav_settings'] = 'Alert Settings';
-$l['myalerts_usercp_nav_delete_read'] = 'Delete Old Alerts';
-$l['myalerts_usercp_nav_delete_all'] = 'Delete All Alerts';
+$l['myalerts_usercp_nav'] = 'Уведомления';
+$l['myalerts_usercp_nav_alerts'] = 'Показать уведомления';
+$l['myalerts_usercp_nav_settings'] = 'Параметры уведомлений';
+$l['myalerts_usercp_nav_delete_read'] = 'Удалить старые уведомления';
+$l['myalerts_usercp_nav_delete_all'] = 'Удалить все уведомления';
 
-$l['myalerts_error_alert_not_found'] = 'Alert not found.';
+$l['myalerts_error_alert_not_found'] = 'Уведомление не найдено.';
 
-$l['myalerts_delete'] = 'Delete';
+$l['myalerts_delete'] = 'Удалить;
