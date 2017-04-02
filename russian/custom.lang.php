fbf6bca42e1085eccf8988c49fc3bcd637efa01e
Ulysses <udevide@gmail.com>
2017-04-02 14:43:33 -0400

    Update custom.lang.php

--- russian/custom.lang.php
+++ russian/custom.lang.php
@@ -4,12 +4,12 @@
  * Date: 23/05/2015
  * Time: 10:33
  */
-$l['private_message'] = '<b>Private shout {1} <span class="private-username">{2}</span></b>: '; // 1 stands for to/from and 2 for the username.
-$l['to'] = 'to';
-$l['from'] = 'from';
-$l['default_private'] = 'Private shout: ';
-$l['clicktoview'] = "Click to open";
-$l['clicktohide'] = "Click to hide";
-$l['unbanat'] = 'Your ban will be lifted on: {1}';
-$l['shoutbox_reason'] = 'Reason: {1}';
-$l['my_profile'] = 'My Profile';
+$l['private_message'] = '<b>Шепот {1} <span class="private-username">{2}</span></b>: '; // 1 stands for to/from and 2 for the username.
+$l['to'] = 'для';
+$l['from'] = 'от';
+$l['default_private'] = 'Шепот: ';
+$l['clicktoview'] = "Показать";
+$l['clicktohide'] = "Спрятать";
+$l['unbanat'] = 'Вы будете разбанены: {1}';
+$l['shoutbox_reason'] = 'Причина: {1}';
+$l['my_profile'] = 'Мой профиль';
