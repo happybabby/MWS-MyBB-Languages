ef9f00185f0f2a8cead8571664d1f7c5bf3ec229
Tatsuto <tatsuto@modworkshop.net>
2017-06-09 21:25:46 +0300

    Untranslated files and plugins

---
+++ japanese/avatarep.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*  Language English
+*  Last Poster Avatar on Threads and Forums v 2.8.7
+*
+*  Sitio Web: http://forosmybb.es
+*  Autor: Dark Neo - neogeoman@gmail.com
+*/
+$l['avatarep_user_profile'] = "View Profile";
+$l['avatarep_user_messages'] = "View Messages";
+$l['avatarep_user_sendpm'] = "Send PM";
+$l['avatarep_user_sendemail'] = "Send E-mail";
+$l['avatarep_user_threads'] = "View Threads";
+$l['avatarep_user_error'] = " Error ";
+$l['avatarep_user_error_text'] = " You must login to see this content ";
+$l['avatarep_user_alt'] = "{1}'s Avatar";
+$l['avatarep_user_no_avatar'] = "No avatar";
+$l['avatarep_retrieving'] = "Retrieving Data";
+$l['avatarep_loading'] = "Loading...";
+?>
