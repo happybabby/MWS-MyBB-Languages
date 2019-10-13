2049705a51bf322ab98dd198b14105bdc3c863a6
Daniel.R <danielandro11@gmail.com>
2019-10-13 19:41:21 +0300

    Removed old Japanese and Russian

--- russian/dvz_mentions.lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* by Tomasz 'Devilshakerz' Mlynski [devilshakerz.com]; Copyright (C) 2014-2015
- released under Creative Commons BY-NC-SA 4.0 license: http://creativecommons.org/licenses/by-nc-sa/4.0/ */
-
-$l['myalerts_dvz_mentions_post_alert'] = '{1} mentioned you in "{2}".';
-$l['myalerts_setting_dvz_mentions_post'] = 'Получать уведомление, когда о вас упоминают в сообщении?';
