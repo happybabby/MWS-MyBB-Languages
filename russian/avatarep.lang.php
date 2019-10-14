88a7d36a955a6a1cf31c4cb95cf7f1fe070dcdab
happybabby <56563052+happybabby@users.noreply.github.com>
2019-10-14 23:35:48 +0300

    Update avatarep.lang.php

--- russian/avatarep.lang.php
+++ russian/avatarep.lang.php
@@ -6,15 +6,15 @@
 *  Sitio Web: http://forosmybb.es
 *  Autor: Dark Neo - neogeoman@gmail.com
 */
-$l['avatarep_user_profile'] = "View Profile";
-$l['avatarep_user_messages'] = "View Messages";
-$l['avatarep_user_sendpm'] = "Send PM";
-$l['avatarep_user_sendemail'] = "Send E-mail";
-$l['avatarep_user_threads'] = "View Threads";
-$l['avatarep_user_error'] = " Error ";
-$l['avatarep_user_error_text'] = " You must login to see this content ";
+$l['avatarep_user_profile'] = "Посмотреть профиль";
+$l['avatarep_user_messages'] = "Посмотреть сообщения";
+$l['avatarep_user_sendpm'] = "Отправить сообщение";
+$l['avatarep_user_sendemail'] = "Отправить е-майл";
+$l['avatarep_user_threads'] = "Посмотреть треды";
+$l['avatarep_user_error'] = " Ошибка ";
+$l['avatarep_user_error_text'] = " Вы должны войти, чтобы увидеть данный контент ";
 $l['avatarep_user_alt'] = "{1}'s Avatar";
-$l['avatarep_user_no_avatar'] = "No avatar";
-$l['avatarep_retrieving'] = "Retrieving Data";
-$l['avatarep_loading'] = "Loading...";
+$l['avatarep_user_no_avatar'] = "Нет аватара";
+$l['avatarep_retrieving'] = "Получение данных";
+$l['avatarep_loading'] = "Загрузка...";
 ?>
